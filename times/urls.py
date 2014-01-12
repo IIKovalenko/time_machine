3f9d0f2ec52a5828a028d284280dfac79dd85b89
melevir <melevir@gmail.com>
2014-01-12 15:32:41 +0400

    Страница профиля

--- times/urls.py
+++ times/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from times.views import TimeEntryList
+from times.views import TimeEntryList, UserDetailView
 
 urlpatterns = patterns(
     '',
+    url(r'^profile/', UserDetailView.as_view(), name='profile'),
     url(r'^api/time_entry', TimeEntryList.as_view(), name='time_entry_list'),
 )
--- times/views.py
+++ times/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from rest_framework import generics
 from times.models import TimeEntry
 from times.serializers import TimeEntrySerializer
@@ -6,3 +7,13 @@ from times.serializers import TimeEntrySerializer
 class TimeEntryList(generics.ListCreateAPIView):
     queryset = TimeEntry.objects.all()
     serializer_class = TimeEntrySerializer
+
+
+class UserDetailView(TemplateView):
+    template_name = 'times/user_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        user = self.request.user
+        context['entries'] = TimeEntry.objects.filter(user=user)
+        return context
