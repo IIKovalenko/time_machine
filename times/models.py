1236a879a196d394f26ea5a82f517d8ea5202d12
melevir <melevir@gmail.com>
2014-01-13 08:19:20 +0400

    Получение статистики

--- times/models.py
+++ times/models.py
@@ -11,9 +11,22 @@ class TimeEntry(models.Model):
     def __str__(self):
         return '%s %s сек. (%s)' % (self.spent_on, self.time_spend_seconds, str(self.action_type))
 
+    @classmethod
+    def get_statistics(cls, user, date_from, date_to):
+        entries = TimeEntry.objects.filter(user=user, spent_on__range=(date_from, date_to))
+        actions = set([e.action_type for e in entries])
+        total_time_tracked = sum([e.time_spend_seconds for e in entries])
+        time_spend_per_action = {}
+        for action in actions:
+            time_spend_per_action[action] = sum([e.time_spend_seconds for e in entries if e.action_type == action])
+        return {
+            'total_time_tracked': total_time_tracked,
+            'actions_info': time_spend_per_action,
+        }
+
     class Meta:
-        verbose_name = u'Запись о потраченном времени'
-        verbose_name_plural = u'Записи о потраченном времени'
+        verbose_name = 'Запись о потраченном времени'
+        verbose_name_plural = 'Записи о потраченном времени'
 
 
 class ActionType(models.Model):
--- times/views.py
+++ times/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.views.generic import TemplateView
 from rest_framework import generics
 from times.models import TimeEntry
@@ -15,5 +16,5 @@ class UserDetailView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(UserDetailView, self).get_context_data(**kwargs)
         user = self.request.user
-        context['entries'] = TimeEntry.objects.filter(user=user)
+        context['statistics'] = TimeEntry.get_statistics(user, date(2014, 1, 1), date(2014, 1, 20))
         return context
