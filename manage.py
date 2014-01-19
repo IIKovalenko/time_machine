a71672342e41754599ce7ef69b50903e459d0a63
melevir <melevir@gmail.com>
2014-01-19 13:08:39 +0400

    serving settings via django-configurations

--- manage.py
+++ manage.py
@@ -2,9 +2,12 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "time_machine.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'time_machine.settings')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-    from django.core.management import execute_from_command_line
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        os.environ['DJANGO_CONFIGURATION'] = 'Test'
 
+    from configurations.management import execute_from_command_line
     execute_from_command_line(sys.argv)
--- time_machine/settings.py
+++ time_machine/settings.py
@@ -1,75 +1,88 @@
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from configurations import Configuration
 
 
-SECRET_KEY = 'rh+v5cy7i3id%_kn6ljpzp0t&pu4#*f9fp=yr7wtlx-opia^ma'
+class BaseSettings(Configuration):
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-DEBUG = True
+    SECRET_KEY = 'rh+v5cy7i3id%_kn6ljpzp0t&pu4#*f9fp=yr7wtlx-opia^ma'
 
-TEMPLATE_DEBUG = True
+    TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
 
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+        'south',
+        'rest_framework',
 
-    'south',
-    'rest_framework',
-
-    'layout',
-    'times',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'time_machine.urls'
-
-WSGI_APPLICATION = 'time_machine.wsgi.application'
-
-
-DATABASES = {
-    'default': {
-        'NAME': 'time_machine',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'OPTIONS': {'init_command': 'SET storage_engine=INNODB'}
-    }
-}
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-STATIC_URL = '/static/'
-
-TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
-
-LOGIN_URL = '/'
-LOGIN_REDIRECT_URL = '/profile/'
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'layout',
+        'times',
     )
-}
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ROOT_URLCONF = 'time_machine.urls'
+
+    WSGI_APPLICATION = 'time_machine.wsgi.application'
+
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    STATIC_URL = '/static/'
+
+    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+    LOGIN_URL = '/'
+    LOGIN_REDIRECT_URL = '/profile/'
+
+    REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.IsAuthenticated',
+        )
+    }
+
+
+class Dev(BaseSettings):
+    DEBUG = True
+    DATABASES = {
+        'default': {
+            'NAME': 'time_machine',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'OPTIONS': {'init_command': 'SET storage_engine=INNODB'}
+        }
+    }
+
+
+class Test(BaseSettings):
+    DATABASES = {
+        'default': {
+            'NAME': ':memory:',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'USER': '',
+            'PASSWORD': '',
+        }
+    }
